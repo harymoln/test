703b2ad4f7feea9503923b027e6355d8291ec32a
harymoln <44670630+harymoln@users.noreply.github.com>
2018-11-02 11:28:59 +0800

    创建java文件
    第一次在github上写java程序

---
+++ java1.java
@@ -0,0 +1,14 @@
+public class  test1{
+    private string name;
+    
+    public static void main(string[] args){
+    
+    name="zhangsan";
+    
+    system.out.println(name+"第一次在github上写程序");
+    
+    }
+
+
+}
+
